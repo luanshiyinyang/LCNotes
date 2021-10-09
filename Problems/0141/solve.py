d0da27779fbdc4fbf3010d234fe10164a0bf7072
luanshiyinyang <luanshiyinyang@gmail.com>
2021-10-09 13:26:45 +0800

    update 141

--- Problems/0141/solve.py
+++ Problems/0141/solve.py
@@ -6,10 +6,10 @@
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        p1, p2 = head, head;
+        p1, p2 = head, head
         while p2 != None and p2.next != None:
-            p1 = p1.next;
-            p2 = p2.next.next;
+            p1 = p1.next
+            p2 = p2.next.next
             if p1 == p2:
                 return True
         return False
